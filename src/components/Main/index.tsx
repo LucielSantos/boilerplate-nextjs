cb363b9f62bbdffd253a8a70f99646e51704a7ed
LucielVitorBS <luciel.vitor.bs@gmail.com>
2022-02-17 22:29:41 -0300

    add styled-components create main page style

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,5 +1,20 @@
+import * as Styles from './styles'
+
 export const Main = () => (
-  <main>
-    <h1>React avançado</h1>
-  </main>
+  <Styles.Wrapper>
+    <Styles.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado"
+    />
+    <Styles.Title>React avançado</Styles.Title>
+
+    <Styles.Description>
+      TypeScript, ReactJS, NextJS e Styled Components
+    </Styles.Description>
+
+    <Styles.Illustration
+      src="img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para tela com código"
+    />
+  </Styles.Wrapper>
 )
