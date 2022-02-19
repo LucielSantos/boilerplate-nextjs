6288715841053bcdc05dcb7f2925c8e95a1b0549
LucielVitorBS <luciel.vitor.bs@gmail.com>
2022-02-18 21:05:53 -0300

    add and config storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,16 +1,17 @@
 import * as Styles from './styles'
 
-export const Main = () => (
+export const Main = ({
+  title = 'React avançado',
+  description = 'TypeScript, ReactJS, NextJS e Styled Components'
+}) => (
   <Styles.Wrapper>
     <Styles.Logo
       src="/img/logo.svg"
       alt="Imagem de um átomo e React Avançado escrito ao lado"
     />
-    <Styles.Title>React avançado</Styles.Title>
+    <Styles.Title>{title}</Styles.Title>
 
-    <Styles.Description>
-      TypeScript, ReactJS, NextJS e Styled Components
-    </Styles.Description>
+    <Styles.Description>{description}</Styles.Description>
 
     <Styles.Illustration
       src="img/hero-illustration.svg"
